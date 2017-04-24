ab2fea14e344b7078a9c99e9d8a1697af3b0ab67
147403 <liuhui@syswin.com>
2017-04-24 11:22:51 +0800

    局部注册

--- route1.js
+++ route1.js
@@ -1,3 +1,7 @@
+var Home = {template:"<div>home template content</div>"}
+
+var About = {template:"<div>about template content</div>"}
+
 //组件定义
 var App = Vue.extend({           
     template:"#app_template"
@@ -5,5 +9,9 @@ var App = Vue.extend({
 
 //组件实例化，挂载组件
 var app = new App({
-    el:"#app"
+    el:"#app",
+    components:{
+        'home':Home,  //局部组件注册之快捷方式
+        'about':About
+    }
 })
