83c20d78bb5b4765d10830d5b15fb5960f22e4d9
147403 <liuhui@syswin.com>
2017-04-24 11:36:21 +0800

    简单路由

--- route1.js
+++ route1.js
@@ -2,6 +2,11 @@ var Home = {template:"<div>home template content</div>"}
 
 var About = {template:"<div>about template content</div>"}
 
+var routes = {
+    '/':Home,
+    '/about':About
+}
+
 //组件定义
 var App = Vue.extend({           
     template:"#app_template"
